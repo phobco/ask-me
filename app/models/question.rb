b3d95290231d9557ddb96f7313dd59d33fc5a2f2
Philipp Obrivanov <milliardex@gmail.com>
2021-11-11 14:40:17 +0300

    Remove comments

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,9 +2,5 @@ class Question < ApplicationRecord
   belongs_to :user
 
   validates :user, :text, presence: true
-
-  # ДЗ
-  #
-  # 4. Проверка максимальной длины текста вопроса (максимум 255 символов)
   validates :text, length: { maximum: 255 }
 end
