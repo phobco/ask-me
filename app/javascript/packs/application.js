06a701e34c8ba11fbcae54e9749fcffdf9e3f3c7
Philipp Obrivanov <milliardex@gmail.com>
2021-11-15 18:43:31 +0300

    Update js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,3 +6,17 @@
 import Rails from "@rails/ujs"
 
 Rails.start()
+
+document.addEventListener('DOMContentLoaded', () => {
+  const askButton = document.getElementById('ask-button')
+  const askForm = document.getElementById('ask-form')
+
+  askButton.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    setTimeout(() => {
+      const displayStyle = askForm.style.display === 'block' ? 'none' : 'block'
+      askForm.style.display = displayStyle
+    }, 300)
+  })
+})
