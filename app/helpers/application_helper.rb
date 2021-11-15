ce0948055adc5c364311f9fdd3e915f6033eb32f
Philipp Obrivanov <milliardex@gmail.com>
2021-11-15 22:35:10 +0300

    Add count_question method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,16 @@ module ApplicationHelper
       asset_path 'avatar.png'
     end
   end
+
+  def count_questions(number)
+    if (number % 100).between?(11, 14)
+      "#{number} вопросов"
+    elsif number % 10 == 1
+      "#{number} вопрос"
+    elsif (number % 10).between?(2,4)
+      "#{number} вопроса"
+    else
+      "#{number} вопросов"
+    end
+  end
 end
