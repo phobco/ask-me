a7de2105a1002f3fd30dd533b2384dbe169d49c0
Philipp Obrivanov <milliardex@gmail.com>
2021-11-16 22:40:51 +0300

    Add plurals for answers and unanswered questions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
       asset_path 'avatar.png'
     end
   end
+
+  def count_answers
+    @user.questions.count(&:answer)
+  end
+
+  def unanswered_questions
+    @questions.size - count_answers
+  end
 end
