2a48e6db475971a84c0e861fcaa8fcb3d2685f5f
Philipp Obrivanov <milliardex@gmail.com>
2021-11-17 17:38:13 +0300

    Add create and update methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,52 @@
 class UsersController < ApplicationController
+
+  before_action :load_user, except: [:index, :create, :new]
+
   def index
     @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def edit
   end
 
-  def show
-    @user = User.find(params[:id])
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные обновлены'
+    else
+      render 'edit'
+    end
+  end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+    else
+      render 'new'
+    end
+  end
+
+  def show
     @questions = @user.questions.order(created_at: :desc)
 
-    @new_question = Question.new
+    @new_question = @user.questions.build
 
     @answers = @user.questions.count(&:answer)
     @unanswered_questions = @questions.size - @answers
+  end
+
+  private
+
+  def load_user
+    @user ||= User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url)
   end
 end
