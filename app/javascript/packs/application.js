7478040f3c30a57e9b5e70630efc36e00f2f3eac
Philipp Obrivanov <milliardex@gmail.com>
2021-11-19 16:10:59 +0300

    Add jscolor color picker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require('packs/jscolor')
+
 import Rails from "@rails/ujs"
 
 Rails.start()
