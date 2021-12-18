d37b75c7d90fc3ec148211cac1067ac9cae05e24
Philipp Obrivanov <milliardex@gmail.com>
2021-12-18 19:51:20 +0300

    Add params text

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users
-  resources :questions, except: [:show, :new, :index]
-  resource :session, only: [:new, :create, :destroy]
-  resources :hashtags, only: [:show]
+  resources :questions, except: %i[show new index]
+  resource :session, only: %i[new create destroy]
+  resources :hashtags, only: :show, param: :text
 end
