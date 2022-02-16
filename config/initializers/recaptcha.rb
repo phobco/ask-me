4afa16ac9aa3ad744b491a7da74c7a748891d988
Philipp Obrivanov <milliardex@gmail.com>
2022-02-16 12:19:47 +0300

    Add recaptcha keys to credentials

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key  = ENV['RECAPTCHA_ASKME_PUBLIC_KEY']
-  config.secret_key = ENV['RECAPTCHA_ASKME_PRIVATE_KEY']
+  config.site_key  = Rails.application.credentials.dig(:recaptcha, :site_key)
+  config.secret_key = Rails.application.credentials.dig(:recaptcha, :secret_key)
 end
